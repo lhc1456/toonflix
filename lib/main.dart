407882c761f27f74933a657a0109437f87819eb9
hclim <lsch1456@gmail.com>
2023-10-23 17:35:15 +0900

    3.3 VSCode Settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     // flutter는 구글에서 만들었기 때문에 material이 ios 보다 더 좋은 스타일을 가짐
@@ -15,22 +16,22 @@ class App extends StatelessWidget {
     //google
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: Padding(
           // padding: EdgeInsets.all(10),
           // 수직, 수평 padding 지정
-          padding: EdgeInsets.symmetric(
+          padding: const EdgeInsets.symmetric(
             horizontal: 40,
           ),
           // Column은 위 아래로 쌓고 싶을때 사용
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 80,
               ),
               // Row는 옆에 놓고 싶을때 사용
-              Row(
+              const Row(
                 // MainAxis : Column의 수직방향, Row의 수평방향
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
@@ -58,7 +59,7 @@ class App extends StatelessWidget {
                   )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 120,
               ),
               Text(
@@ -68,10 +69,10 @@ class App extends StatelessWidget {
                   fontSize: 22,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 5,
               ),
-              Text(
+              const Text(
                 '\$5 194 482',
                 style: TextStyle(
                   color: Colors.white,
@@ -79,7 +80,7 @@ class App extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
               Row(
@@ -89,7 +90,7 @@ class App extends StatelessWidget {
                       color: Colors.amber,
                       borderRadius: BorderRadius.circular(45),
                     ),
-                    child: Padding(
+                    child: const Padding(
                       padding: EdgeInsets.symmetric(
                         vertical: 20,
                         horizontal: 50,
