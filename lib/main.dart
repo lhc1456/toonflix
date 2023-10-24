31de92f0e9912ac55d4a3bd4cdfe33373ab05a64
hclim <lsch1456@gmail.com>
2023-10-24 10:03:53 +0900

    3.5 Reusable Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/button.dart';
 
 void main() {
   runApp(const App());
@@ -21,7 +22,7 @@ class App extends StatelessWidget {
           // padding: EdgeInsets.all(10),
           // 수직, 수평 padding 지정
           padding: const EdgeInsets.symmetric(
-            horizontal: 40,
+            horizontal: 20,
           ),
           // Column은 위 아래로 쌓고 싶을때 사용
           child: Column(
@@ -83,25 +84,37 @@ class App extends StatelessWidget {
               const SizedBox(
                 height: 30,
               ),
-              Row(
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(45),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20,
-                        horizontal: 50,
-                      ),
-                      child: Text(
-                        'Transfer',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
+                  Button(
+                    text: 'Transfer',
+                    bgColor: Color(0xFFF1B33B),
+                    textColor: Colors.black,
+                  ),
+                  // Container(
+                  //   decoration: BoxDecoration(
+                  //     color: const Color(0xFF1F2123),
+                  //     borderRadius: BorderRadius.circular(45),
+                  //   ),
+                  //   child: const Padding(
+                  //     padding: EdgeInsets.symmetric(
+                  //       vertical: 20,
+                  //       horizontal: 50,
+                  //     ),
+                  //     child: Text(
+                  //       'Request',
+                  //       style: TextStyle(
+                  //         color: Colors.white,
+                  //         fontSize: 20,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // )
+                  Button(
+                    text: 'Request',
+                    bgColor: Color(0xFF1F2123),
+                    textColor: Colors.white,
                   ),
                 ],
               ),
