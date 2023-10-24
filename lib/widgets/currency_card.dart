16b002c07eafbd604008083bc194d36312ee9036
hclim <lsch1456@gmail.com>
2023-10-24 14:03:52 +0900

    3.9 Code Challenge

--- lib/widgets/currency_card.dart
+++ lib/widgets/currency_card.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class CurrencyCard extends StatelessWidget {
   final String name, code, amount;
   final IconData icon;
-  final bool isInverted;
+  final int order;
 
   final _blackColor = const Color(0xFF1F2123);
 
@@ -13,78 +13,82 @@ class CurrencyCard extends StatelessWidget {
     required this.code,
     required this.amount,
     required this.icon,
-    required this.isInverted,
+    required this.order,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // clipBehavior : overflow 된 내용을 어떻게 할지 선택
-      // Clip.hardEdge : overflow 된 내용 지우기
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: isInverted ? Colors.white : _blackColor,
-        borderRadius: const BorderRadius.all(Radius.circular(20)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    color: isInverted ? _blackColor : Colors.white,
-                    fontSize: 32,
+    bool inverted = (order % 2 == 0);
+    return Transform.translate(
+      offset: Offset(0, order > 0 ? (order - 1) * (-20) : 0),
+      child: Container(
+        // clipBehavior : overflow 된 내용을 어떻게 할지 선택
+        // Clip.hardEdge : overflow 된 내용 지우기
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: inverted ? Colors.white : _blackColor,
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: inverted ? _blackColor : Colors.white,
+                      fontSize: 32,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                          color: inverted ? _blackColor : Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: inverted
+                              ? _blackColor
+                              : Colors.white.withOpacity(0.8),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              // Transform.scale(scale) : scale 배수만큼 크기 증가
+              Transform.scale(
+                scale: 2.2,
+                // Transform.translate(x, y) : x, y 만큼 위치 이동
+                child: Transform.translate(
+                  offset: const Offset(
+                    -5,
+                    15,
+                  ),
+                  child: Icon(
+                    icon,
+                    color: inverted ? _blackColor : Colors.white,
+                    size: 88,
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
-                        color: isInverted ? _blackColor : Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      code,
-                      style: TextStyle(
-                        color: isInverted
-                            ? _blackColor
-                            : Colors.white.withOpacity(0.8),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            // Transform.scale(scale) : scale 배수만큼 크기 증가
-            Transform.scale(
-              scale: 2.2,
-              // Transform.translate(x, y) : x, y 만큼 위치 이동
-              child: Transform.translate(
-                offset: const Offset(
-                  -5,
-                  15,
-                ),
-                child: Icon(
-                  icon,
-                  color: isInverted ? _blackColor : Colors.white,
-                  size: 88,
-                ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
