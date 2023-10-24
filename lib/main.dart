00e5d33ebc4b671b47ae929a538671219d9360de
hclim <lsch1456@gmail.com>
2023-10-24 16:13:41 +0900

    4.2 Recap

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
     // setState(() {}) : Widget에게 새로운 데이터가 있다고 알려줌 -> flutter가 UI를 업데이트(build 재실행)
     // 위에서 데이터를 수정 후 마지막에 setState((){})을 실행해도 가능
     // 하지만 안에 넣는 걸 추천
     setState(() {
-      counter++;
+      numbers.add(numbers.length);
     });
   }
 
@@ -40,12 +40,7 @@ class _AppState extends State<App> {
                   fontSize: 30,
                 ),
               ),
-              Text(
-                '$counter',
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
+              for (var n in numbers) Text('$n'),
               IconButton(
                 iconSize: 40,
                 onPressed: onClicked,
