4229ebdf0b0ceb128d89c2b04f3cb060afd42036
hclim <lsch1456@gmail.com>
2023-10-25 17:18:19 +0900

    6.4 Recap

--- lib/models/webtoon_model.dart
+++ lib/models/webtoon_model.dart
@@ -1,4 +1,14 @@
 class WebtoonModel {
+  /* 
+   * 잘 안쓰이는 패턴
+  // late final String title, thumb, id;
+
+  // WebtoonModel.fromJson(Map<String, dynamic> json) {
+  //   title = json['title'];
+  //   thumb = json['thumb'];
+  //   id = json['id'];
+  // }
+  */
   final String title, thumb, id;
 
   WebtoonModel.fromJson(Map<String, dynamic> json)
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -11,6 +11,8 @@ class ApiService {
   Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
+
+    // async 함수는 Future 타입을 반환함.
     // Future 타입 : 미래에 받을 값의 타입을 알려줌
     // await은 async함수 내에만 존재해야 함
     final response = await http.get(url);
@@ -19,6 +21,8 @@ class ApiService {
       final List<dynamic> webtoons = jsonDecode(response.body);
 
       for (var webtoon in webtoons) {
+        // final instance = WebtoonModel.fromJson(webtoon);
+        // webtoonInstances.add(instance);
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
 
