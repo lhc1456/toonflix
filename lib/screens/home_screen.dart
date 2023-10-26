fe41bb1746cbb08dffe39c4865047fa4815b0899
hclim <lsch1456@gmail.com>
2023-10-26 09:31:09 +0900

    6.7 ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +22,44 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       body: FutureBuilder(
         future: webtoons,
+        // snapshot == futureResult
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text('There is data');
+            // 기본적인 ListView 사용 방식
+            // return ListView(
+            //   children: [
+            //     for (var webtoon in snapshot.data!) Text(webtoon.title)
+            //   ],
+            // );
+            // ListView.builder 사용 방식
+            // 사용자가 볼 수 없는 아이템은 build 하지 않음
+            // return ListView.builder(
+            //   scrollDirection: Axis.horizontal,
+            //   itemCount: snapshot.data!.length,
+            //   itemBuilder: (context, index) {
+            //     // print(index);
+            //     var webtoon = snapshot.data![index];
+            //     return Text(webtoon.title);
+            //   },
+            // );
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                // print(index);
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              // Widget을 리턴, 리스트 아이템 사이에 reder됨
+              // 리스트 아이템들을 구분하기 위해 사용
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 20,
+              ),
+            );
           }
-          return const Text('Loading...');
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
