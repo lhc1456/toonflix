0b5231f05f7ebebf7c4e49ad6b2a6dafa9e2f200
hclim <lsch1456@gmail.com>
2023-10-26 13:08:53 +0900

    6.11 Recap

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -13,22 +13,27 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // GestureDetector : 탭, 마우스이동, 드래그, 줌 등 감지 가능
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
+          // MaterialPageRoute : 새로운 화면 호출
           MaterialPageRoute(
             builder: (context) => DetailScreen(
               title: title,
               thumb: thumb,
               id: id,
             ),
+            // 어플의 경우 아래에서 위로 나타나며 X표시로 새로운 화면을 닫으려면 true
+            // 오른쪽에서 왼쪽으로 나타나며 뒤로가기로 새로운 화면을 닫으려면 삭제 또는 false(defalut)
             fullscreenDialog: true,
           ),
         );
       },
       child: Column(
         children: [
+          // 사용자가 클릭한 ID와 새로운 화면의 ID를 일치시켜 새로운 지점으로 떠가는 것처럼 함.
           Hero(
             tag: id,
             child: Container(
